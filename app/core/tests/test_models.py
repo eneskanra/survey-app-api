568a7d51d184e8e11339c540afb02270915751da
enes <eneskanra@gmail.com>
2020-05-03 13:46:58 +0300

    Refactor the user admin test to pass the test.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-from core import models
-
 
 def sample_user(email='sample@example.com', password='testpass'):
     """Create a sample user"""
